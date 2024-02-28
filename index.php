0877600c52254644adee30b0c2d0df5a910c01c1
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-02-28 15:59:06 +0100

    feat (all) /start

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!-- Consegna: creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
